ddff86bdb9eb2c2da4c407313e1b767ccad6a5d3
Cristian Daniel Cornejo Pereira <cristian.cornejo1@outlook.com>
2022-08-08 19:57:19 -0500

    feat: add routes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+   <div class="home">
+    <h1>This is the page that creates a game</h1>
+  </div>
 </template>
