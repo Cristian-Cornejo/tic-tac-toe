f5d122d00e324dea7f63a59de89c8c8f8c0de0e7
Cristian Daniel Cornejo Pereira <cristian.cornejo1@outlook.com>
2022-08-14 22:34:24 -0500

    feat: add tile component

---
+++ src/components/tile.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+const { selectedBy } = defineProps<{ selectedBy: string | null }>();
+</script>
+
+<template>
+    <button class="game-tile" v-bind:class="{ pressed: !!selectedBy }">{{
+            selectedBy
+    }}</button>
+</template>
+
+<style scoped>
+.game-tile {
+    transform: translateY(-8px);
+    width: 72px;
+    height: 72px;
+    border: none;
+    color: var(--white-color);
+    font-weight: bolder;
+    font-size: 28px;
+    transition: all 0.2s;
+}
+
+.game-tile:nth-child(1) {
+    border-top-left-radius: 8px;
+}
+
+.game-tile:nth-child(3) {
+    border-top-right-radius: 8px;
+}
+
+.game-tile:nth-child(7) {
+    border-bottom-left-radius: 8px;
+}
+
+.game-tile:nth-child(9) {
+    border-bottom-right-radius: 8px;
+}
+
+.game-tile:nth-child(2n+1):hover {
+    transform: translateY(0px);
+    box-shadow: 0px 0px var(--dark-green-color);
+}
+
+.game-tile:nth-child(2n):hover {
+    transform: translateY(0px);
+    box-shadow: 0px 0px var(--dark-blue-color);
+}
+
+.game-tile:nth-child(2n+1).pressed {
+    transform: translateY(0px);
+    filter: brightness(0.85);
+    box-shadow: 0px 0px var(--dark-green-color);
+}
+
+.game-tile:nth-child(2n).pressed {
+    transform: translateY(0px);
+    filter: brightness(0.85);
+    box-shadow: 0px 0px var(--dark-blue-color);
+}
+
+.game-tile:nth-child(2n+1) {
+    background-color: var(--green-color);
+    box-shadow: 0px 8px var(--dark-green-color);
+}
+
+.game-tile:nth-child(2n) {
+    background-color: var(--blue-color);
+    box-shadow: 0px 8px var(--dark-blue-color);
+}
+</style>
--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -2,15 +2,13 @@
   <div class="game-view">
     <h2>Player 1 turn</h2>
     <div class="container game-board">
-      <button class="game-tile" v-bind:class="{ pressed: !!tile.selectedBy }" v-for="tile in tiles" :key="tile.id">{{
-          tile.selectedBy
-      }}</button>
+      <Tile :selectedBy="tile.selectedBy" v-for="tile in tiles" :key="tile.id"></Tile>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
-
+import Tile from '../components/tile.vue'
 const tiles = ref([
   { id: 0, selectedBy: null },
   { id: 1, selectedBy: 'X' },
@@ -37,64 +35,5 @@ const tiles = ref([
   grid-template-columns: repeat(3, 1fr);
   background-color: var(--light-pink-color);
   box-shadow: 0px 8px var(--pink-color);
-}
-
-.game-tile {
-  transform: translateY(-8px);
-  width: 72px;
-  height: 72px;
-  border: none;
-  color: var(--white-color);
-  font-weight: bolder;
-  font-size: 28px;
-  transition: all 0.2s;
-}
-
-.game-tile:nth-child(1) {
-  border-top-left-radius: 8px;
-}
-
-.game-tile:nth-child(3) {
-  border-top-right-radius: 8px;
-}
-
-.game-tile:nth-child(7) {
-  border-bottom-left-radius: 8px;
-}
-
-.game-tile:nth-child(9) {
-  border-bottom-right-radius: 8px;
-}
-
-.game-tile:nth-child(2n+1):hover {
-  transform: translateY(0px);
-  box-shadow: 0px 0px var(--dark-green-color);
-}
-
-.game-tile:nth-child(2n):hover {
-  transform: translateY(0px);
-  box-shadow: 0px 0px var(--dark-blue-color);
-}
-
-.game-tile:nth-child(2n+1).pressed {
-  transform: translateY(0px);
-  filter: brightness(0.85);
-  box-shadow: 0px 0px var(--dark-green-color);
-}
-
-.game-tile:nth-child(2n).pressed {
-  transform: translateY(0px);
-  filter: brightness(0.85);
-  box-shadow: 0px 0px var(--dark-blue-color);
-}
-
-.game-tile:nth-child(2n+1) {
-  background-color: var(--green-color);
-  box-shadow: 0px 8px var(--dark-green-color);
-}
-
-.game-tile:nth-child(2n) {
-  background-color: var(--blue-color);
-  box-shadow: 0px 8px var(--dark-blue-color);
 }
 </style>
