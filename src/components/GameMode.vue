ccf6bf46d379af5cc3d8b30d14570da433f3aaf9
Cristian Daniel Cornejo Pereira <cristian.cornejo1@outlook.com>
2022-08-14 22:34:59 -0500

    feat: add GameMod component

---
+++ src/components/GameMode.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const isVsPlayer = ref(true);
+
+</script>
+<template>
+    <div class="game-mode">
+        <p>PLAYER VS.</p>
+        <button class="game-mode__vs-player-button" v-on:click="!isVsPlayer && (isVsPlayer = !isVsPlayer)"
+            v-bind:class="{ pressed: isVsPlayer }">PLAYER</button>
+        <button class="game-mode__vs-bot-button" v-on:click="isVsPlayer && (isVsPlayer = !isVsPlayer)"
+            v-bind:class="{ pressed: !isVsPlayer }">BOT</button>
+    </div>
+</template>
+<style scoped>
+.game-mode {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    border-radius: 8px;
+    justify-content: center;
+    align-content: center;
+}
+
+.game-mode p {
+    grid-column: 1 / 3;
+    margin: 0;
+    justify-content: center;
+    text-align: center;
+    padding: 4px;
+    font-weight: bold;
+}
+
+.game-mode button {
+    padding: 4px 8px;
+    width: 100%;
+    border: none;
+    font-weight: bold;
+}
+
+.game-mode p {
+    background-color: var(--green-color);
+    box-shadow: 0px 8px var(--dark-green-color);
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+}
+
+.game-mode__vs-player-button {
+    background-color: var(--blue-color);
+    box-shadow: 0px 8px var(--dark-blue-color);
+    transition: all 0.2s;
+}
+
+.game-mode__vs-player-button.pressed {
+    transform: translateY(8px);
+    box-shadow: 0px 0px var(--dark-blue-color);
+    color: var(--white-color);
+}
+
+.game-mode__vs-bot-button {
+    background-color: var(--green-color);
+    box-shadow: 0px 8px var(--dark-green-color);
+    transition: all 0.2s;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+}
+
+
+.game-mode__vs-bot-button.pressed {
+    transform: translateY(8px);
+    box-shadow: 0px 0px var(--dark-green-color);
+    color: var(--white-color);
+}
+</style>
--- src/views/StartMenu.vue
+++ src/views/StartMenu.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-const isVsPlayer = ref(true);
+import GameMode from '../components/GameMode.vue';
 const isXMarkSelected = ref(true);
 
 </script>
@@ -9,13 +9,7 @@ const isXMarkSelected = ref(true);
   <div class="centered-column start-menu">
     <h1 class="start-menu__title">TIC TAC TOE</h1>
     <div class="container centered-column start-menu__mark-selector">
-      <div class="game-mode">
-        <p>PLAYER VS.</p>
-        <button class="game-mode__vs-player-button" v-on:click="!isVsPlayer && (isVsPlayer = !isVsPlayer)"
-          v-bind:class="{ pressed: isVsPlayer }">PLAYER</button>
-        <button class="game-mode__vs-bot-button" v-on:click="isVsPlayer && (isVsPlayer = !isVsPlayer)"
-          v-bind:class="{ pressed: !isVsPlayer }">BOT</button>
-      </div>
+      <GameMode />
       <h2>Pick player 1's mark</h2>
       <div class="centered-row mark-selector__toggler">
         <button class="container start-menu__button start-menu__x-button"
@@ -32,23 +26,6 @@ const isXMarkSelected = ref(true);
 </template>
 
 <style scoped>
-.game-mode {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  border-radius: 8px;
-  justify-content: center;
-  align-content: center;
-}
-
-.game-mode p {
-  grid-column: 1 / 3;
-  margin: 0;
-  justify-content: center;
-  text-align: center;
-  padding: 4px;
-  font-weight: bold;
-}
-
 .start-menu__start-button {
   border: none;
   font-weight: bolder;
@@ -63,47 +40,6 @@ const isXMarkSelected = ref(true);
 .start-menu__start-button:hover {
   box-shadow: 0px 0px var(--dark-green-color);
   transform: translateY(8px);
-}
-
-.game-mode button {
-  padding: 4px 8px;
-  width: 100%;
-  border: none;
-  font-weight: bold;
-}
-
-.game-mode p {
-  background-color: var(--green-color);
-  box-shadow: 0px 8px var(--dark-green-color);
-  border-top-left-radius: 8px;
-  border-bottom-left-radius: 8px;
-}
-
-.game-mode__vs-player-button {
-  background-color: var(--blue-color);
-  box-shadow: 0px 8px var(--dark-blue-color);
-  transition: all 0.2s;
-}
-
-.game-mode__vs-player-button.pressed {
-  transform: translateY(8px);
-  box-shadow: 0px 0px var(--dark-blue-color);
-  color: var(--white-color);
-}
-
-.game-mode__vs-bot-button {
-  background-color: var(--green-color);
-  box-shadow: 0px 8px var(--dark-green-color);
-  transition: all 0.2s;
-  border-top-right-radius: 8px;
-  border-bottom-right-radius: 8px;
-}
-
-
-.game-mode__vs-bot-button.pressed {
-  transform: translateY(8px);
-  box-shadow: 0px 0px var(--dark-green-color);
-  color: var(--white-color);
 }
 
 .start-menu__button {
