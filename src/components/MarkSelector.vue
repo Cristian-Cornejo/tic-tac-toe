da4fc7860e838054e4a4063c47feeb9dbd55fd59
Cristian Daniel Cornejo Pereira <cristian.cornejo1@outlook.com>
2022-08-14 22:47:37 -0500

    feat: add mark selector component

---
+++ src/components/MarkSelector.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const isXMarkSelected = ref(true);
+
+</script>
+
+<template>
+    <div class="centered-row mark-selector__toggler">
+        <button class="container start-menu__button start-menu__x-button"
+            v-on:click="!isXMarkSelected && (isXMarkSelected = !isXMarkSelected)"
+            v-bind:class="{ pressed: isXMarkSelected }"><span>x</span></button>
+        <button class="container start-menu__button start-menu__o-button"
+            v-on:click="isXMarkSelected && (isXMarkSelected = !isXMarkSelected)"
+            v-bind:class="{ pressed: !isXMarkSelected }">o</button>
+    </div>
+</template>
+<style scoped>
+.mark-selector__toggler {
+    width: 100%;
+}
+
+.mark-selector__toggler button {
+    width: 80px;
+    height: 40px;
+    border: none;
+}
+
+.mark-selector__toggler button+button {
+    margin-left: 8px;
+}
+
+.start-menu__button {
+    font-weight: bolder;
+    font-size: 28px;
+    padding: 4px;
+    transition: all 0.2s;
+}
+
+.start-menu__x-button {
+    background-color: var(--blue-color);
+    color: var(--dark-blue-color);
+    box-shadow: 0px 8px var(--dark-blue-color);
+}
+
+.start-menu__x-button.pressed {
+    box-shadow: 0px 0px var(--dark-blue-color);
+    color: var(--white-color);
+    transform: translateY(8px);
+}
+
+.start-menu__o-button {
+    background-color: var(--green-color);
+    color: var(--dark-blue-color);
+    box-shadow: 0px 8px var(--dark-green-color);
+}
+
+.start-menu__o-button.pressed {
+    box-shadow: 0px 0px var(--dark-green-color);
+    transform: translateY(8px);
+    color: var(--white-color);
+}
+</style>
--- src/views/StartMenu.vue
+++ src/views/StartMenu.vue
@@ -1,24 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import GameMode from '../components/GameMode.vue';
-const isXMarkSelected = ref(true);
-
+import MarkSelector from '../components/MarkSelector.vue';
 </script>
 
 <template>
-  <div class="centered-column start-menu">
+  <div class="centered-column">
     <h1 class="start-menu__title">TIC TAC TOE</h1>
-    <div class="container centered-column start-menu__mark-selector">
+    <div class="container centered-column start-menu__wrapper">
       <GameMode />
       <h2>Pick player 1's mark</h2>
-      <div class="centered-row mark-selector__toggler">
-        <button class="container start-menu__button start-menu__x-button"
-          v-on:click="!isXMarkSelected && (isXMarkSelected = !isXMarkSelected)"
-          v-bind:class="{ pressed: isXMarkSelected }"><span>x</span></button>
-        <button class="container start-menu__button start-menu__o-button"
-          v-on:click="isXMarkSelected && (isXMarkSelected = !isXMarkSelected)"
-          v-bind:class="{ pressed: !isXMarkSelected }">o</button>
-      </div>
+      <MarkSelector />
       <p>remember: x goes first</p>
       <button class="container start-menu__start-button">START!</button>
     </div>
@@ -42,51 +33,6 @@ const isXMarkSelected = ref(true);
   transform: translateY(8px);
 }
 
-.start-menu__button {
-  font-weight: bolder;
-  font-size: 28px;
-  padding: 4px;
-  transition: all 0.2s;
-}
-
-.start-menu__x-button {
-  background-color: var(--blue-color);
-  color: var(--dark-blue-color);
-  box-shadow: 0px 8px var(--dark-blue-color);
-}
-
-.start-menu__x-button.pressed {
-  box-shadow: 0px 0px var(--dark-blue-color);
-  color: var(--white-color);
-  transform: translateY(8px);
-}
-
-.start-menu__o-button {
-  background-color: var(--green-color);
-  color: var(--dark-blue-color);
-  box-shadow: 0px 8px var(--dark-green-color);
-}
-
-.start-menu__o-button.pressed {
-  box-shadow: 0px 0px var(--dark-green-color);
-  transform: translateY(8px);
-  color: var(--white-color);
-}
-
-.mark-selector__toggler {
-  width: 100%;
-}
-
-.mark-selector__toggler button {
-  width: 80px;
-  height: 40px;
-  border: none;
-}
-
-.mark-selector__toggler button+button {
-  margin-left: 8px;
-}
-
 .start-menu__title {
   font-size: 2rem;
   font-weight: 700;
@@ -94,17 +40,17 @@ const isXMarkSelected = ref(true);
   color: var(--white-color);
 }
 
-.start-menu__mark-selector {
+.start-menu__wrapper {
   background-color: var(--light-pink-color);
   box-shadow: 0px 8px var(--pink-color);
   padding: 32px 24px;
 }
 
-.start-menu__mark-selector h2 {
+.start-menu__wrapper h2 {
   color: var(--yellow-color);
 }
 
-.start-menu__mark-selector p {
+.start-menu__wrapper p {
   color: var(--dark-blue-color);
 }
 </style>
